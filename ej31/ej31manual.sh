480ec73170bbe02e1e15e7671169130ed6bdadba
Lucas López <lopezlucas730@gmail.com>
2025-12-05 18:45:00 -0300

    Ejercicio 31 hecho de forma "manual"
    Create a script to manage user directories and subdirectories.

---
+++ ej31/ej31manual.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Realice un script que reciba como argumento una lista de posibles nombres de
+# usuarios del sistema y, para cada uno de los que efectivamente existan en el
+# sistema y posean un directorio personal configurado que sea válido, realice las
+# modificaciones necesarias en su directorio personal para que tenga un
+# subdirectorio llamado “directorio_iso” con la estructura del cuadro.
+
+if [ $# -lt 1 ]; then
+   exit 1
+fi
+
+for usuario in "$@"; do
+  usu=$(cut -d : -f1 /etc/passwd | grep "^$usuario$")
+  if [ "$usuario" == "$usu" ]; then #verifico si el usuario existe
+      home=$(grep "^$usuario:" /etc/passwd | cut -d : -f6)  #obtengo directorio personal de usuario 
+      if [ -d "$home" ]; then  #verifico que home exista
+          mkdir -p "$home/directorio_iso"
+          mkdir -p "$home/directorio_iso"/{2025..2026}/{01..12}
+          touch "$home/directorio_iso"/{2025..2026}/{01..12}/archivo.txt
+      fi
+  fi
+done
+exit 0
